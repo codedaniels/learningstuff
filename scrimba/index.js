a69a26a6e77d38d4023d84a21194ee07f3ae80f7
Daniel S <danielksstone@gmail.com>
2018-12-20 20:54:15 -0800

    index updated

--- scrimba/index.js
+++ scrimba/index.js
@@ -2,4 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // JSX
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
+ReactDOM.render(<div>\<h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+
+var myNewP = document.createElement("p")
+myNewP.innerHTML = "This is a paragraph."
