7d01099364f15dba0210627cb86a716b542a240e
Daniel S <danielksstone@gmail.com>
2018-12-22 17:54:20 -0800

    updating index js to render to dom

--- scrimba/index.js
+++ scrimba/index.js
@@ -2,7 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // JSX
-ReactDOM.render(<div>\<h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+// ReactDOM.render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
 
-var myNewP = document.createElement("p")
-myNewP.innerHTML = "This is a paragraph."
+// var myNewP = document.createElement("p")
+// myNewP.innerHTML = "This is a paragraph."
+
+function myApp () {
+    return 
+}
+
+ReactDOM.render(
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>,
+    document.getElementById("root")
+  )
